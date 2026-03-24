64d9f35c1f7fe93ed2291a3ca3cf0d9b5f32e683
Epuwatellybenon <epuwatelly@gmail.com>
2026-03-24 20:36:41 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,21 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Epuwat Elly Benon*/
+/* 25/U/BIE/18668/PE */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float distance_travelled, fuel_used, fuel_efficiency;
 
+    printf("Enter distance(km):\n");
+    scanf("%f", & distance_travelled);
+
+    printf("Enter fuel used(litres):\n");
+    scanf("%f", &fuel_used);
+
+    fuel_efficiency= distance_travelled/fuel_ueed;
+
+    printf("fuel_efficiency:%.2f km/l\n", fuel_efficiency);
+    
     return 0;
 }
